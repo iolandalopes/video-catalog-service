ea45d6cb69bc3cf80379bbde11992bd533a37512
Iolanda Lopes <iolandalopes@MacBook-Air-de-Iolanda.local>
2026-01-03 15:23:46 -0300

    feat: add category entity

---
+++ src/Core/Domain/Entity/Category.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Core\Domain\Entity;
+
+use Core\Domain\Entity\Traits\MagicMethodsTrait;
+
+class Category
+{
+    use MagicMethodsTrait;
+
+    public function __construct(
+        protected string $id = '',
+        protected string $name = '',
+        protected string $description = '',
+        protected bool $isActive = true,
+    ) {}
+}
