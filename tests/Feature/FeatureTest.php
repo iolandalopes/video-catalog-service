fe9184d049779b42eb74283cde4e333dd4b6b0db
Iolanda Lopes <iolandalopes@MacBook-Air-de-Iolanda.local>
2026-01-03 07:07:23 -0300

    chore(test): add phpunit

---
+++ tests/Feature/FeatureTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class FeatureTest extends TestCase
+{
+    public function testShouldAssertTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/UnitTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UnitTest extends TestCase
+{
+    public function testShouldAssertTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
