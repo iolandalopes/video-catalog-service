4c82eedc05b0cf9379b8ebde93bc1defc9a6f384
Iolanda Lopes <iolandalopes@MacBook-Air-de-Iolanda.local>
2026-01-03 16:19:57 -0300

    feat: add update method to category entity

--- src/Core/Domain/Entity/Category.php
+++ src/Core/Domain/Entity/Category.php
@@ -24,4 +24,10 @@ class Category
     {
         $this->isActive = false;
     }
+
+    public function update(string $name, string $description): void
+    {
+        $this->name = $name;
+        $this->description = $description;
+    }
 }
